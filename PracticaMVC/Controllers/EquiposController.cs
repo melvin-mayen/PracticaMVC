5d3cfa8ff06bec27aa76e8f18dc99a73f7b6ac98
2020MZ602@unicaeselsalvador0.onmicrosoft.com <2020MZ602@unicaeselsalvador0.onmicrosoft.com>
2024-04-05 20:05:12 -0600

    Agregar archivos de proyecto.

---
+++ PracticaMVC/Controllers/EquiposController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PracticaMVC.Models;
+
+namespace PracticaMVC.Controllers
+{
+    public class EquiposController : Controller
+    {
+        private readonly equiposDbContext _equiposDbContext;
+
+        public EquiposController(equiposDbContext equiposDbContext)
+        {
+            _equiposDbContext = equiposDbContext;
+        }
+
+
+        public IActionResult CrearEquipos(equipos nuevoEquipo)
+        {
+            _equiposDbContext.Add(nuevoEquipo);
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Index()
+        {
+            var listaDeMarcas = (from m in _equiposDbContext.marcas
+                                 select m).ToList();
+            ViewData["listadoDeMarcas"] = new SelectList(listaDeMarcas, "id_marcas", "nombre_marca");
+
+            return View();
+        }
+    }
+}
---
+++ PracticaMVC/Models/equiposDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticaMVC.Models
+{
+    public class equiposDbContext : DbContext
+    {
+
+        public equiposDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<marcas> marcas { get; set; }
+
+    }
+}
